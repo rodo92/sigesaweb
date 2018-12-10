9ded93bb620e4ee2f72a2d71fe8e387aed755517
Rodolfo Crisanto Rosas <crisanto.rosas.22@gmail.com>
2018-12-10 15:17:32 -0500

    Configuracion de Tamaño de TIcket FE

--- app/Http/Controllers/PDFTicketController.php
+++ app/Http/Controllers/PDFTicketController.php
@@ -55,16 +55,16 @@ class PDFTicketController extends Controller
 	
 	public function cabecera_ticket()
     {
-    	$this->fpdf->Image('svg/Logos/logo-factura.jpg',0,5,20);
-		$this->fpdf->SetFont('Arial','B',7);
-		$this->fpdf->SetXY(17,5);
+    	$this->fpdf->Image('svg/Logos/logo-factura.jpg',0,5,1);
+		$this->fpdf->SetFont('Arial','B',8);
+		$this->fpdf->SetXY(10,5);
 		$this->fpdf->Cell(55,5,'HOSPITAL NACIONAL ARZOBISPO LOAYZA',0,1,'L');
-		$this->fpdf->SetFont('Arial','',7);
-		$this->fpdf->SetXY(20,9);
+		$this->fpdf->SetFont('Arial','',8);
+		$this->fpdf->SetXY(13,9);
 		$this->fpdf->Cell(64,4,'Av. Alfonso Ugarte 848 - Telefono 6144646',0,1,'L');
-		$this->fpdf->SetXY(30,13);
+		$this->fpdf->SetXY(23,13);
         $this->fpdf->Cell(30,4,'RUC: 20154996991',0,1,'L');
-        $this->fpdf->SetXY(20,16);
+        $this->fpdf->SetXY(13,16);
         $this->fpdf->Cell(50,5,"TELF:6144650 para citas de lunes-viernes",0,1,'C');
         $this->fpdf->SetXY(4,25);
         $this->fpdf->SetFont('Arial','B',8);
@@ -73,7 +73,7 @@ class PDFTicketController extends Controller
 
     public function datos_receptor()
     {
-    	$this->fpdf->SetXY(5,32);
+    	$this->fpdf->SetXY(10,32);
 		$this->fpdf->SetFillColor(248,249,249);
 		$this->fpdf->SetFont('Arial','B',9);
 		$this->fpdf->Cell(40,4,$this->cabecera[0]['NroSerie'] . "-" . $this->cabecera[0]['NroDocumento'],0, 0, 'L', True);
@@ -81,7 +81,7 @@ class PDFTicketController extends Controller
 		$this->fpdf->SetFont('Arial','',7);
 		$this->fpdf->SetXY(40,32);
 		$this->fpdf->Cell(110,4,$this->fecha_emision,0, 0, 'L', True);
-		$this->fpdf->SetXY(5,36);
+		$this->fpdf->SetXY(10,36);
 		$this->fpdf->Cell(40,4,'Usuario :',0, 0, 'L', True);
 		$this->fpdf->SetXY(20,36);
 		$this->fpdf->Cell(110,4,utf8_decode($this->cabecera[0]['ApellidoPaterno'] . ' ' . $this->cabecera[0]['ApellidoMaterno'] . ' ' . $this->cabecera[0]['PrimerNombre']),0, 0, 'L', True);
@@ -90,10 +90,10 @@ class PDFTicketController extends Controller
     public function detalle_factura()
     {
     	$this->fpdf->Ln(5);
-    	$this->fpdf->SetX(2);
+    	$this->fpdf->SetX(7);
     	$this->fpdf->SetFont('Arial','B',7);
 		$this->fpdf->Cell(8,5,utf8_decode('COD'),0,0,'C');
-		$this->fpdf->Cell(38,5,utf8_decode('DESCRIPCIÓN'),0,0,'L');
+		$this->fpdf->Cell(30,5,utf8_decode('DESCRIPCIÓN'),0,0,'L');
 		$this->fpdf->Cell(9,5,utf8_decode('CANT'),0,0,'C');
 		$this->fpdf->Cell(7,5,utf8_decode('PU'),0,0,'C');
 		$this->fpdf->Cell(7,5,utf8_decode('PT'),0,1,'C');
@@ -103,9 +103,9 @@ class PDFTicketController extends Controller
 		for ($i=0; $i < count($this->detalle); $i++) { 
 
 			$h =  $h + 1;
-			$this->fpdf->SetX(2);
+			$this->fpdf->SetX(7);
 			$this->fpdf->Cell(8,5,trim($this->detalle[$i]['Codigo']),'',0,'C');
-			$this->fpdf->Cell(38,5,utf8_decode($this->detalle[$i]['Descripcion']),'',0,'L');
+			$this->fpdf->Cell(30,5,utf8_decode($this->detalle[$i]['Descripcion']),'',0,'L');
 			$this->fpdf->Cell(9,5,$this->detalle[$i]['Cantidad'],'',0,'C');
 			$this->fpdf->Cell(7,5,number_format($this->detalle[$i]['ValorUnitario'],2,'.',' '),'',0,'C');
 			$this->fpdf->Cell(7,5,number_format($this->detalle[$i]['SubTotal'],2,'.',' '),'',1,'C');
@@ -114,9 +114,9 @@ class PDFTicketController extends Controller
 				$this->fpdf->AddPage('P','A4');
 			}
 		}
-		$this->fpdf->SetX(2);
+		$this->fpdf->SetX(7);
 		$this->fpdf->Cell(8,1,"",'B',0,'C');
-		$this->fpdf->Cell(38,1,"",'B',0,'C');
+		$this->fpdf->Cell(30,1,"",'B',0,'C');
 		$this->fpdf->Cell(9,1,"",'B',0,'L');
 		$this->fpdf->Cell(7,1,"",'B',0,'C');
 		$this->fpdf->Cell(7,1,"",'B',1,'C');
@@ -124,13 +124,13 @@ class PDFTicketController extends Controller
 		// Datos Adicionales
 		$this->fpdf->Ln(1);
 		$this->fpdf->SetX(2);
-		$this->fpdf->Cell(62,5,utf8_decode('Sub Total: '),0,0,'R');
+		$this->fpdf->Cell(57,5,utf8_decode('Sub Total: '),0,0,'R');
 		$this->fpdf->Cell(7,5,number_format($this->cabecera[0]['Subtotal'],2,'.',' '),0,1,'C');
 		$this->fpdf->SetX(2);
-		$this->fpdf->Cell(62,5,utf8_decode('Adelantos: '),0,0,'R');
+		$this->fpdf->Cell(57,5,utf8_decode('Adelantos: '),0,0,'R');
 		$this->fpdf->Cell(7,5,number_format('0.00',2,'.',' '),0,1,'C');
 		$this->fpdf->SetX(2);
-		$this->fpdf->Cell(62,5,utf8_decode('Total: '),0,0,'R');
+		$this->fpdf->Cell(57,5,utf8_decode('Total: '),0,0,'R');
 		$this->fpdf->Cell(7,5,number_format($this->cabecera[0]['Total'],2,'.',' '),0,1,'C');
 
 		
@@ -139,17 +139,17 @@ class PDFTicketController extends Controller
     public function pie_factura()
 	{
 	    $this->fpdf->SetY(-17);
-	    $this->fpdf->SetX(2);
+	    $this->fpdf->SetX(7);
 	    $codigo = $this->cabecera[0]['Ruc'] . '|' . $this->cabecera[0]['NroSerie'] . '|' . trim($this->cabecera[0]['NroDocumento']) . '|' . $this->cabecera[0]['Subtotal'] . '|' . $this->cabecera[0]['IGV'] . '|' . $this->cabecera[0]['Total'];
 
 	    $this->fpdf->Image(route('qrsimple',$codigo) . trim($codigo),25,103,25,25,'PNG');
-	    $this->fpdf->SetX(2);
+	    $this->fpdf->SetX(7);
 		$this->fpdf->Cell(40,4,'Resolucion: Nro. 0340050010017/SUNAT',0, 0, 'L', True);
 		$this->fpdf->Ln(3);
-		$this->fpdf->SetX(2);
+		$this->fpdf->SetX(7);
 		$this->fpdf->Cell(40,4,'Consulta en: https://escondatagafe.page.link/bon',0, 0, 'L', True);
 		$this->fpdf->Ln(3);
-		$this->fpdf->SetX(2);
+		$this->fpdf->SetX(7);
 		$this->fpdf->SetFont('Arial','B',8);
 		$this->fpdf->Cell(40,4,'Gracias por su visita',0, 0, 'L', True);
 	}
